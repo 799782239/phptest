90c11ad98b0fda1cc444915272bed23b3ad3eb76
799782239 <799782239@qq.com>
2017-01-16 19:32:47 +0800

    与前端初步调试

--- asd.php
+++ asd.php
@@ -6,11 +6,13 @@
  * Time: 17:00
  */
 include 'TestObj.php';
+$sku = $_GET['skuIds'];
+$type = $_GET['type'];
 $a = array('a' => 1, 'b' => 2);
 $obj = new TestObj();
-$obj->name = 'a';
+$obj->name = $sku;
 $obj->id = '11';
-$obj->money = '152';
+$obj->money = $type;
 
 $b = json_encode($obj);
 echo $b;
